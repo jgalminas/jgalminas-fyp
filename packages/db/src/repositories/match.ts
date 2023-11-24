3c5a590b41e4dd0e8303c1134bcddc2bc9b96193
Justas Galminas <j.galminas@gmail.com>
2023-11-24 18:04:36 +0000

    insert match wip

--- packages/db/src/repositories/match.ts
+++ packages/db/src/repositories/match.ts
@@ -1,8 +1,25 @@
+import { db } from "../db";
 import { IMatch } from "../schema";
+import { Frame } from "../schema/frame";
 import { Match } from "../schema/match";
 
 export const insertMatch = async(data: IMatch) => {
-  const match = await Match.create(data);
-  await match.save();
+
+  const session = await (await db).startSession();
+  session.startTransaction();
+
+  let match;
+
+  try {
+    const frames = await Frame.insertMany(data.frames, { session });
+    // data.frames = frames;
+    match = await Match.create(data, { session });
+    await session.commitTransaction();
+    session.endSession();
+  } catch (err) {
+    console.log(err);
+    await session.abortTransaction();
+  }
+
   return match;
 } 
