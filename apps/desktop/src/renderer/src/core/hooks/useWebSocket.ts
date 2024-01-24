75c368475c1feec0cc3449fd88c69691d0004970
Justas Galminas <j.galminas@gmail.com>
2024-01-24 09:04:26 +0000

    reconnection logic + reusable connection in useWebSocket hook

--- apps/desktop/src/renderer/src/core/hooks/useWebSocket.ts
+++ apps/desktop/src/renderer/src/core/hooks/useWebSocket.ts
@@ -1,27 +1,55 @@
 import env from "@root/env";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 /**
  * A custom hook to create a socket connection
  */
+
+let socket: WebSocket | null = null;
+let subscribers = 0;
+
 export const useWebSocket = () => {
 
-    const [socket, setSocket] = useState<WebSocket | null>(null);
+    let retries = 0;
+
+    const connect = () => {
+
+        if (socket === null) {
+            const ws = new WebSocket(env.RENDERER_VITE_SOCKET_URL);
+        
+            ws.onopen = () => {
+                socket = ws;
+                retries = 0;
+            }
+    
+            ws.onclose = (event) => {
+                if (event.code !== 1000 && event.code !== 1006 && ws.readyState !== WebSocket.OPEN) {
+                    reconnect();
+                }
+            }
+        }
+
+    } 
+
+    const reconnect = () => {
+        setTimeout(() => {
+            retries += 1;
+            connect();
+        }, 1000 * Math.pow(2, retries))
+    }
 
     useEffect(() => {
-
-        (async() => {
-            const ws = new WebSocket(env.RENDERER_VITE_SOCKET_URL);
-            ws.onopen = () => {
-                setSocket(ws);
-            }
-        })();
+        subscribers += 1;
+        connect();
 
         return () => {
-            socket?.close();
+            subscribers -= 1;
+            if (subscribers === 0) {
+                socket?.close(1000);
+            }
         }
  
     }, [])
 
-    return { socket }
+    return { socket: socket }
 }
