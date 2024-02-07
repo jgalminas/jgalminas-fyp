1d2f7c3bb2980f70076b62fb76fe564d29fbcea0
Justas Galminas <j.galminas@gmail.com>
2024-02-07 15:19:47 +0000

    tooltip shadow

--- apps/desktop/src/renderer/src/core/WithTooltip.tsx
+++ apps/desktop/src/renderer/src/core/WithTooltip.tsx
@@ -26,7 +26,7 @@ export const WithTooltip = ({ children, tooltip, className, arrowClass }: WithTo
               "data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade",
               "text-violet11 select-none rounded-[4px] bg-woodsmoke-100 px-[15px] py-[10px]",
               "text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]",
-              "will-change-[transform,opacity] text-star-dust-200 text-sm z-50",
+              "will-change-[transform,opacity] text-star-dust-200 text-sm z-50 drop-shadow-md",
               className
               )}
             sideOffset={5}
