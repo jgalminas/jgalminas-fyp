009b171548310be736ce79a409ef106786e05405
Justas Galminas <j.galminas@gmail.com>
2024-02-07 14:37:47 +0000

    make tabs sticky on scroll rather than the whole page header

--- apps/desktop/src/renderer/src/pages/match/Match.tsx
+++ apps/desktop/src/renderer/src/pages/match/Match.tsx
@@ -30,15 +30,20 @@ const Match = () => {
   return (
     <Page header={<DefaultHeader back="/matches"/>}
     pageClass="max-w-[80rem]" contentClass="gap-0">
-      <PageInnerHeader className="sticky top-0 bg-woodsmoke-900 z-10 gap-0">
+
+      <PageInnerHeader className="gap-0">
         <PageTitle> Match Details </PageTitle>
         <div className="flex items-center gap-5 mt-1">
           <p className="text-star-dust-300 font-medium"> { queue(data.queueId) } </p>
           <p className="text-star-dust-400"> { formatMatchLength(data.start, data.finish) } </p>
         </div>
-        <Tabs className="mt-8" tabs={tabs}/>
-        <Divider/>
       </PageInnerHeader>
+
+      <div className="mt-8 sticky top-0 z-10 bg-woodsmoke-900">
+        <Tabs tabs={tabs}/>
+        <Divider/>
+      </div>
+
       <PageBody className="pb-5 pt-8">
         <Outlet context={{ match: data }}/>
       </PageBody>
