3709840e9e367ee6f1fbf9ba9b5aeea9ee65e78e
Justas Galminas <j.galminas@gmail.com>
2024-02-21 14:32:34 +0000

    refactored bar charts & added tooltips on hover

--- apps/desktop/src/renderer/src/pages/match/components/BarChart.tsx
+++ apps/desktop/src/renderer/src/pages/match/components/BarChart.tsx
@@ -1,86 +1,69 @@
-import { cn } from "@fyp/class-name-helper";
 import Card from "@renderer/core/Card";
-import { ReactNode } from "react";
+import { Asset } from "@renderer/util/asset";
+import { ResponsiveContainer, BarChart as ReBarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar, Cell } from "recharts";
+
 
 type Data = {
-  value: number,
-  label: ReactNode,
-  color: string // tailwind color class
-}[];
-
-export type BarChartProps = {
-  className?: string,
   label: string,
-  data: Data,
-  tickCount?: number,
-  height: number
-  barWidth?: number,
+  value: number,
+  color: string
 }
 
-const BarChart = ({ label, className, data, height, tickCount = 5, barWidth = 20 }: BarChartProps) => {
+export type BarChartProps = {
+  label: string,
+  data: Data[],
+  height: number
+}
 
-  const findHighestValue = <T extends { value: number }>(array: T[]) => {
-    return array.reduce((max, current) => {
-      return current.value > max ? current.value : max;
-    }, array[0].value);
-  }
+export const BarChart = ({ data, height, label }: BarChartProps) => {
 
-  const largest = (Math.ceil((findHighestValue(data) / 5) / 1000) * 1000) * tickCount;
-  const scale = height / largest;
-  const tickInterval = (largest * scale) / tickCount;
+  const CustomTick = (props: { x: number, y: number, payload: { index: number, value: string } }) => {
+    const { x, y, payload } = props;
+  
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <defs >
+          <clipPath id="rounded">
+            <rect width={36} height={36} rx={18} ry={18} x={-18} y={8}/>
+          </clipPath>
+        </defs>
+        <image x={-18} y={8} width={36} height={36} href={Asset.champion(payload.value)} clipPath="url(#rounded)"/>
+        <circle cx="0" cy="26" r="18" fill="none" stroke={data[payload.index].color} strokeWidth="2"/>
+      </g>
+    );
+  };
+
+  const tickFormatter = (num: number) => num >= 1000 ? `${Math.round(num / 1000)}K` : num.toString();
 
   return (
-    <Card className={cn("flex flex-col text-sm", className)}>
-      <p className="mb-4 text-star-dust-300 uppercase font-medium text-xs"> { label } </p>
-      <div className="grid gap-x-2 relative place-items-center items-end pl-12 pr-4 mt-2"
-      style={{ height: height, gridTemplateColumns: `repeat(${data.length},minmax(0,1fr))` }}>
-
-      { data.map((value, i) => {
-          return (
-            <div key={i} className={cn("z-10", value.color)}
-            style={{ height: `${((value.value * scale) / (largest * scale)) * 100}%`, width: barWidth }}>
-              &nbsp;
-            </div>
-          )
-      }) }
-
-     {
-        Array.from({ length: (height / tickInterval) + 1 }).map((_, i) => {
-          const tick = i * tickInterval / scale;
-          return (
-            <div key={i} className="absolute w-full items-center text-star-dust-400 gap-2 bottom-0 left-0"
-            style={{ bottom: i * tickInterval }}>
-              <p className="absolute -translate-y-1/2">
-                { tick >= 1000 ? `${Math.round(tick / 1000)}K` : tick }
-              </p>
-              <div className="ml-8 bottom-1/2 bg-woodsmoke-400 h-[1px] w-[100% - 2rem]"/>
-            </div>  
-          )
-        })
-      }
-      </div>
-      <Labels data={data}/>
+    <Card className="pl-0">
+      <p className="pl-5 mb-4 text-star-dust-300 uppercase font-medium text-xs"> { label } </p>
+      <ResponsiveContainer width="100%" height={height}>
+        <ReBarChart data={data} margin={{ bottom: 32, top: 16, right: 8 }} maxBarSize={16}>
+          <CartesianGrid vertical={false} stroke="#202328"/>
+          <XAxis dataKey="label" tickLine={false} tick={CustomTick} interval={0} axisLine={false}/>
+          <YAxis interval={0} tickLine={false} axisLine={false} tick={{ fill: '#999999' }} tickFormatter={tickFormatter}/>
+          <Tooltip
+          cursor={{ fill: '#1D2024' }}
+          itemStyle={{
+            color: "#D1D1D1",
+            fontWeight: 400
+          }}
+          contentStyle={{
+            backgroundColor: "#22262B",
+            borderRadius: 8,
+            border: "none",
+            color: "#D1D1D1",
+            fontSize: 14,
+            fontWeight: 700
+          }}/>
+          <Bar dataKey="value">
+            { data.map((_, index) => (
+              <Cell key={index} fill={data[index].color}/>
+            ))}
+          </Bar>
+        </ReBarChart>
+      </ResponsiveContainer>
     </Card>
   )
 }
-
-type LabelsProps = {
-  data: Data
-}
-
-const Labels = ({ data }: LabelsProps) => {
-  return (
-    <div className="grid gap-x-2 pl-12 pr-4 place-items-center w-full"
-    style={{ gridTemplateColumns: `repeat(${data.length},minmax(0,1fr))` }}>
-      { data.map(({ label }, i) => {
-        return (
-          <div key={i} className="row-start-2">
-            { label }
-          </div>
-        )
-      }) }
-    </div>
-  )
-}
-
-export default BarChart;
--- apps/desktop/src/renderer/src/pages/match/tabs/Scoreboard.tsx
+++ apps/desktop/src/renderer/src/pages/match/tabs/Scoreboard.tsx
@@ -1,10 +1,11 @@
 import { useOutletContext } from "react-router";
 import ScoreboardTable from "../components/ScoreboardTable";
 import { IParticipant, Match } from "@fyp/types";
-import BarChart from "../components/BarChart";
+// import BarChart from "../components/BarChart";
 import RoundImage from "@renderer/core/RoundImage";
 import { Asset } from "@renderer/util/asset";
 import { cn } from "@fyp/class-name-helper";
+import { BarChart } from "../components/BarChart";
 
 const ScoreBoard = () => {
 
@@ -14,11 +15,8 @@ const ScoreBoard = () => {
     const participant = match.participants.find((p) => p.participantId === value.participantId) as IParticipant;
     return {
       value: value.physicalDamageToChampions + value.magicDamageToChampions + value.trueDamageToChampions,
-      label: (
-        <RoundImage src={Asset.champion(participant.champion)}
-        className={cn("mt-4 h-10 w-10", participant.team === 'BLUE' ? "border-accent-blue" : "border-accent-red")}/>
-      ),
-      color: participant.team === 'BLUE' ? 'bg-accent-blue' : 'bg-accent-red'
+      label: participant.champion,
+      color: participant.team === 'BLUE' ? '#0068CA' : '#FF3B3A'
     }
   }).sort((a, b) => a.value > b.value ? -1 : 1);
 
@@ -26,19 +24,48 @@ const ScoreBoard = () => {
     const participant = match.participants.find((p) => p.participantId === value.participantId) as IParticipant;
     return {
       value: value.totalGold,
-      label: (
-        <RoundImage src={Asset.champion(participant.champion)}
-        className={cn("mt-4 h-10 w-10", participant.team === 'BLUE' ? "border-accent-blue" : "border-accent-red")}/>
-      ),
-      color: participant.team === 'BLUE' ? 'bg-accent-blue' : 'bg-accent-red'
+      label: participant.champion,
+      color: participant.team === 'BLUE' ? '#0068CA' : '#FF3B3A'
     }
   }).sort((a, b) => a.value > b.value ? -1 : 1);
+
+  const data = [
+    {
+      label: "Akshan",
+      value: 2002,
+      color: 'red'
+    },
+    {
+      label: "Fizz",
+      value: 20022,
+      color: 'red'
+    }
+  ]
+
+
 
   return (
     <div className="grid grid-cols-2 gap-6 grid-rows-[auto,auto]">
       <ScoreboardTable className="col-span-full" match={match}/>
-      <BarChart className="h-fit" height={200} data={damage} label="Damage Distribution"/>
-      <BarChart className="h-fit" height={200} data={income} label="Income"/>
+      <BarChart height={280} data={damage} label="Damage Distribution"/>
+      <BarChart height={280} data={income} label="Income"/>
+
+      {/* <ResponsiveContainer width="100%" height={240}>
+        <BarChart data={data} margin={{ bottom: 32, top: 16 }} maxBarSize={16}>
+          <CartesianGrid vertical={false} stroke="black"/>
+          <XAxis dataKey="name" tickLine={false} tick={CustomTick} interval={0} axisLine={false}/>
+          <YAxis interval={0} tickLine={false} axisLine={false} tick={{ fill: '#000' }}/>
+          <Tooltip contentStyle={{ backgroundColor: "#fff", borderRadius: 8 }}/>
+          <Bar dataKey="pv" fill="#8884d8">
+          { data.map((entry, index) => (
+            <Cell key={index} fill={entry.name === "Akshan" ? '#290a0a' : '#005599' }/>
+          ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer> */}
+
+      {/* <BarChart className="h-fit" height={200} data={damage} label="Damage Distribution"/>
+      <BarChart className="h-fit" height={200} data={income} label="Income"/> */}
     </div>
   )
 }
