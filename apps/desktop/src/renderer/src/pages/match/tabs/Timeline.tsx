68c4b1e0e777b62bb28b1a3ea8e8c8ab6962bdc8
Justas Galminas <j.galminas@gmail.com>
2024-02-23 12:18:11 +0000

    timeline tab loading indicator

--- apps/desktop/src/renderer/src/pages/match/tabs/Timeline.tsx
+++ apps/desktop/src/renderer/src/pages/match/tabs/Timeline.tsx
@@ -4,12 +4,13 @@ import { ClientRequestBuilder } from '@renderer/util/request';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router';
 import { GoldAdvantageChart } from '../components/GoldAdvantageChart';
+import Loading from '@renderer/core/Loading';
 
 const Timeline = () => {
 
   const { matchId } = useParams();
 
-  const { data } = useQuery<MatchWithGoldFrames>({
+  const { data, isLoading } = useQuery<MatchWithGoldFrames>({
     queryKey: ['gold', matchId],
     queryFn: async() => {
       const res = await new ClientRequestBuilder()
@@ -23,9 +24,11 @@ const Timeline = () => {
     <div>
       <Card className='overflow-x-hidden flex flex-col'>
         <p className="mb-4 text-star-dust-300 uppercase font-medium text-xs"> Team Gold Advantage </p>
-        { data
+        { !isLoading ?
+          data
           ? <GoldAdvantageChart data={data}/>
           : null
+          : <Loading className="my-72"/>
         }
       </Card>
     </div>
